fc06657d9b0d459f22966aaac2105403297daa05
Max Deineko <max.deineko@gmail.com>
2015-10-22 10:48:47 +0200

    Rename.

--- bin/pkg.sh
+++ bin/pkg.sh
@@ -11,5 +11,5 @@ trap "exit 1" INT QUIT TERM
 PROJ_DIR=`realpath ${0%/*}/..`
 
 #"${PROJ_DIR}/src/rstsp/build_mingw.sh" || exit 1
-"${PROJ_DIR}/src/rstsp/pkg.sh"
+"${PROJ_DIR}/src/rstsp/pkg_mingw.sh"
 
