dae47190486a611a64e51838c56b5f63f6c4b530
Max Deineko <max.deineko@gmail.com>
2015-11-05 00:11:01 +0100

    Fixed random test interval.

--- test/test_random.sh
+++ test/test_random.sh
@@ -18,7 +18,7 @@ LOG_DIR="${TEST_DIR}"/log
 mkdir -p "${DATA_DIR}" | exit 1
 mkdir -p "${LOG_DIR}" | exit 1
 
-for i in {0..20}; do
+for i in {1..20}; do
   "${TEST_DIR}"/random.rb $i 1 10 > "${DATA_DIR}"/random.$i
   (time "${PROJ_DIR}"/src/rstsp/build/rstsp.poly 0 "${DATA_DIR}"/random.$i )\
     > "${LOG_DIR}"/log_random_$i 2>&1
