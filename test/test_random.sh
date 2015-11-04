3f0f35746c6cd749d7ac9a11d0a4d9e9d249f80c
Max Deineko <max.deineko@gmail.com>
2015-11-04 21:32:08 +0100

    Added random tests.

---
+++ test/test_random.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+#
+# $File$
+# $Author$
+# $Date$
+# $Revision$
+#
+
+trap "exit 1" INT QUIT TERM
+
+PROJ_DIR=`realpath ${0%/*}/..`
+TEST_DIR="${PROJ_DIR}"/test
+DATA_DIR="${TEST_DIR}"/data/random
+LOG_DIR="${TEST_DIR}"/log
+
+#"${PROJ_DIR}/src/rstsp/build_native.sh" || exit 1
+
+mkdir -p "${DATA_DIR}" | exit 1
+mkdir -p "${LOG_DIR}" | exit 1
+
+for i in {0..20}; do
+  "${TEST_DIR}"/random.rb $i 1 10 > "${DATA_DIR}"/random.$i
+  (time "${PROJ_DIR}"/src/rstsp/build/rstsp.poly 0 "${DATA_DIR}"/random.$i )\
+    > "${LOG_DIR}"/log_random_$i 2>&1
+done
+
+grep -iE 'real|types' "${LOG_DIR}"/log_random_* >> "${LOG_DIR}"/log_random_all
+
