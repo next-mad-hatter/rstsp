d0825cc1cc176a70c6720c033645cc636dff38ae
Max Deineko <max.deineko@gmail.com>
2015-11-23 00:57:45 +0100

    Cosmetics

--- bin/plot.sh
+++ bin/plot.sh
@@ -12,13 +12,11 @@ PROJ_DIR=`realpath ${0%/*}/..`
 cd "${PROJ_DIR}/plot" || exit 1
 mkdir -p out || exit 1
 
-echo -n "Checking ruby version and libraries: "
 $( ruby -e "if RUBY_VERSION >= \"1.9.3\" then print \"OK\" else exit(1) end" > /dev/null 2>&1 )
 if [ $? -ne 0 ]; then
-  echo "not found"
+  echo "No suitable ruby installation found."
   exit 1
 fi
-echo "ok"
 
 ./plot_num_types.gpi
 
