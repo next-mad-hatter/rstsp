a0d360ec79d92a5cf155c98c4434b44e973006d2
Max Deineko <max.deineko@gmail.com>
2015-11-26 11:57:19 +0100

    data stats : format details added

--- bin/data_stats.sh
+++ bin/data_stats.sh
@@ -12,6 +12,14 @@ DATA_DIR="${PROJ_DIR}"/test/data
 . "${PROJ_DIR}"/bin/check_ruby.sh
 
 grep DIMENSION "${DATA_DIR}"/**/*.tsp "${DATA_DIR}"/random/* | \
-  ruby -e 'x = STDIN.readlines.map(&:strip).map{|y| y.split(/[\s:]+/)}; puts x.map{|y| ["", y[-1], y[0]].join("\t")}.join("\n")' | \
-  sort -V
+  ruby -e 'x = STDIN.readlines.map(&:strip).map{|y| y.split(/[\s:]+/)}
+           puts x.map{|y|
+             open(y[0]){|f| f.find{|l| l =~ /EDGE_WEIGHT_TYPE[\s:]+(\S+)/}}
+             wt = $1
+             open(y[0]){|f| f.find{|l| l =~ /EDGE_WEIGHT_FORMAT[\s:]+(\S+)/}}
+             wf = $1
+             [y[-1], wt || "", wf || "-", y[0]]
+           }.sort_by{|r| r[0].to_i}
+            .map{|r| ["",r[0].rjust(8), r[1].ljust(10), r[2].rjust(16), r[3]].join("\t")}
+            .join("\n")'
 
