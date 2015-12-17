e09317c2c8909e386927bd61d3befe1ead9aa14b
Max Deineko <max.deineko@gmail.com>
2015-12-17 23:21:06 +0100

    Report draft: pyramidal tours added.

--- report/build.sh
+++ report/build.sh
@@ -13,15 +13,23 @@ trap "exit 1" INT QUIT TERM
 
 cd "${REPORT_DIR}"
 pandoc -f rst -t latex ../doc/sml-support.rst  > sml-support.tex
-# This is purely for convenience.
 ctags -Ra .
 
-#
-# Three (sic!) latex runs should be enough for references to settle.
-#
-cd "${REPORT_DIR}"
-lualatex -shell-escape -interaction=nonstopmode "${SUBMISSION}".tex || exit 1
-biber "${SUBMISSION}".bcf
-lualatex "${SUBMISSION}".tex
-exec lualatex "${SUBMISSION}".tex
+../src/rstsp/build/rstsp.mlton -o /dev/null -t p ../test/data/random/random.5 -l ../plot/data/trace_pyr.dot
+../src/rstsp/build/rstsp.mlton -o /dev/null -t b ../test/data/random/random.5 -l ../plot/data/trace_bal.dot
+dot -Tpdf ../plot/data/trace_pyr.dot -o ../plot/out/trace_pyr.pdf
+dot -Tpdf ../plot/data/trace_bal.dot -o ../plot/out/trace_bal.pdf
+
+#cd "${REPORT_DIR}"
+wget -nc https://imgs.xkcd.com/comics/travelling_salesman_problem.png -O tsp.png
+
+ERR="$(lualatex -shell-escape -interaction=nonstopmode -file-line-error "${SUBMISSION}".tex)"
+STATUS="$?"
+echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
+if [ $STATUS -ne 0 ]; then
+  exit 1
+fi
+biber -q "${SUBMISSION}".bcf
+lualatex -shell-escape -interaction=batchmode -file-line-error "${SUBMISSION}".tex
+texfot lualatex -shell-escape "${SUBMISSION}".tex
 
