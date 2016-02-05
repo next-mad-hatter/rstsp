7ec7342bd0587a8354e23aa51a86281266b2afa4
Max Deineko <max.deineko@gmail.com>
2016-02-05 03:23:49 +0100

    Fixed s.b. hash base regression.

--- report/build.sh
+++ report/build.sh
@@ -18,28 +18,28 @@ ctags -Ra .
 #cd "${REPORT_DIR}"
 wget -nc https://imgs.xkcd.com/comics/travelling_salesman_problem.png -O tsp.png
 
-## REPORT
-# ERR="$(lualatex -shell-escape -interaction=nonstopmode -file-line-error "${SUBMISSION}".tex)"
-# STATUS="$?"
-# echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
-# if [ $STATUS -ne 0 ]; then
-#   exit 1
-# fi
-# biber -q "${SUBMISSION}".bcf
-# # xindy is broken on my machine
-# makeindex "${SUBMISSION}".idx
-# makeglossaries "${SUBMISSION}"
-# lualatex -shell-escape -interaction=batchmode -file-line-error "${SUBMISSION}".tex
-# texfot lualatex -shell-escape "${SUBMISSION}".tex
-
-## SLIDES
-ERR="$(latex -shell-escape -interaction=nonstopmode -file-line-error slides.tex)"
+# REPORT
+ERR="$(lualatex -shell-escape -interaction=nonstopmode -file-line-error "${SUBMISSION}".tex)"
 STATUS="$?"
 echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
 if [ $STATUS -ne 0 ]; then
   exit 1
 fi
-latex -shell-escape -interaction=batchmode -file-line-error slides.tex
-texfot latex -shell-escape slides.tex
-dvips slides.dvi
-ps2pdf slides.ps
+biber -q "${SUBMISSION}".bcf
+# xindy is broken on my machine
+makeindex "${SUBMISSION}".idx
+makeglossaries "${SUBMISSION}"
+lualatex -shell-escape -interaction=batchmode -file-line-error "${SUBMISSION}".tex
+texfot lualatex -shell-escape "${SUBMISSION}".tex
+
+## SLIDES
+#ERR="$(latex -shell-escape -interaction=nonstopmode -file-line-error slides.tex)"
+#STATUS="$?"
+#echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
+#if [ $STATUS -ne 0 ]; then
+#  exit 1
+#fi
+#latex -shell-escape -interaction=batchmode -file-line-error slides.tex
+#texfot latex -shell-escape slides.tex
+#dvips slides.dvi
+#ps2pdf slides.ps
