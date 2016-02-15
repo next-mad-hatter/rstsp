22e65f25db28af282dd65754cbf6410d48fdd43d
Max Deineko <max.deineko@gmail.com>
2016-02-16 00:28:55 +0100

    Report: basic definitions & local search reworked.

--- report/build.sh
+++ report/build.sh
@@ -33,13 +33,13 @@ lualatex -shell-escape -interaction=batchmode -file-line-error "${SUBMISSION}".t
 texfot lualatex -shell-escape "${SUBMISSION}".tex
 
 ## SLIDES
-ERR="$(latex -shell-escape -interaction=nonstopmode -file-line-error slides.tex)"
-STATUS="$?"
-echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
-if [ $STATUS -ne 0 ]; then
-  exit 1
-fi
-latex -shell-escape -interaction=batchmode -file-line-error slides.tex
-texfot latex -shell-escape slides.tex
-dvips slides.dvi
-ps2pdf slides.ps
+#ERR="$(latex -shell-escape -interaction=nonstopmode -file-line-error slides.tex)"
+#STATUS="$?"
+#echo "${ERR}" | egrep ".*:[0-9]*:.*|Warning:"
+#if [ $STATUS -ne 0 ]; then
+#  exit 1
+#fi
+#latex -shell-escape -interaction=batchmode -file-line-error slides.tex
+#texfot latex -shell-escape slides.tex
+#dvips slides.dvi
+#ps2pdf slides.ps
